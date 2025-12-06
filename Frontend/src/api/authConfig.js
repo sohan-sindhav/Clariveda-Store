deabd479baf02724fff536ab6cc7045d220ede9b
sohan <sohann7704@gmail.com>
2025-12-06 15:05:34 +0530

    backend changes

--- Frontend/src/api/authConfig.js
+++ Frontend/src/api/authConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://clariveda-store.onrender.com:5000";
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
