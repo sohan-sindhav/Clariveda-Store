1b2b6aa6569d38286162f415eeedc158ef623731
sohan <sohann7704@gmail.com>
2025-12-06 14:58:00 +0530

    initial changes

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,9 @@ app.use(
   })
 );
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://clariveda-store.vercel.app/", credentials: true })
+);
 
 app.use(
   rateLimit({
